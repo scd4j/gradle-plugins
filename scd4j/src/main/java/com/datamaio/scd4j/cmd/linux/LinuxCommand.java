81be9eeccbdce3feae9afbed64090c559772d8b3
mateusmcosta <mmdcosta@gmail.com>
2015-03-20 19:24:18 -0300

    FixtText does not work on windows.

--- scd4j/src/main/java/com/datamaio/scd4j/cmd/linux/LinuxCommand.java
+++ scd4j/src/main/java/com/datamaio/scd4j/cmd/linux/LinuxCommand.java
@@ -129,19 +129,24 @@ public abstract class LinuxCommand extends Command {
 
 	@Override
 	public void fixTextContent(String file) {
+		boolean runDos2unix = true;
+		
 		if(!Files.exists(Paths.get("/usr/bin/dos2unix"))) {
 			if(whoami().equals("root")) {
 				installRemotePack("dos2unix");
 			}else {
+				runDos2unix = false;
 				replaceLineSeparator(file);
 			}
 		}
-					
-		List<String> cmd = new ArrayList<String>();
-		cmd.add("dos2unix");
-		cmd.add(file);
 		
-		run(cmd, false);
+		if(runDos2unix){
+			List<String> cmd = new ArrayList<String>();
+			cmd.add("dos2unix");
+			cmd.add(file);
+			
+			run(cmd, false);
+		}
 	}
 	
 	@Override
